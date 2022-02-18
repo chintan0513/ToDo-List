8c049370e83e42cd62aed4bc1bb5f9cdd5a0609a
chintan tripathi <chintantripathi1310@gmail.com>
2022-02-18 21:57:14 +0530

    todo UI done

---
+++ script.js
@@ -0,0 +1,50 @@
+const input = document.querySelector('.search')
+const btn = document.querySelector("#add")
+const list = document.querySelector('#list_item')
+
+// console.log(input, btn, list)
+
+input.addEventListener('keydown', e => {
+    const key = e.key
+    if(key === 'Enter') {
+        console.log('Enter Key');
+        handleAdd()
+    }
+})
+const handleAdd = (e) => {
+    // console.log('Adding an event')
+    const textToAdd = input.value
+    if(textToAdd.length>0) {
+        const ele = document.createElement('li')
+        const text_node = document.createTextNode(textToAdd)
+        ele.appendChild(text_node)
+        list.appendChild(ele)
+
+        //create a dynamic button element
+        const button = document.createElement('button')
+        button.innerHTML = "Delete"
+        ele.appendChild(button)
+
+        //clear the list
+        button.addEventListener('click', (e) => {
+            ele.remove()
+            // console.log('button to delete')
+        })
+        input.value = ''
+    }
+    else {
+        window.alert("task cant be edit")
+    }
+    
+}
+btn.addEventListener('click', (handleAdd))
+
+// dark mode
+
+const dark_btn = document.querySelector('label')
+const body = document.querySelector('body')
+
+dark_btn.addEventListener('click', (e) => {
+    // body.classList.toggle('dark')
+    body.classList.add('dark')
+})
